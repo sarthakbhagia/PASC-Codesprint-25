a43111f616f04e3ed0e7cba5c1bdcd1bbe77878a
sarthakbhagia <sarthakbhagia1@gmail.com>
2025-11-23 21:16:06 +0530

    AddHoursAndMinutesToTime.cpp

--- AddHoursAndMinutesToTime.cpp
+++ AddHoursAndMinutesToTime.cpp
@@ -3,25 +3,42 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <iomanip>
 using namespace std;
 
-
 int main() {
-    int HH,MM,hourincrement,minuteincrement;
-    char colon=':';
+    
+    int HH,MM,h_add,m_add;
+    char colon;
 
-    cin>>HH>>colon>>MM;
-  
-    cin>>hourincrement>>minuteincrement;
- 
-    MM = MM + minuteincrement;
+    if(!(cin>>HH>>colon>>MM)||colon!=':'){
+        cout<<"use numbers only"<<endl;
+        return 0;
+    }
+    if(!(cin>>h_add>>m_add)){
+        cout<<"use numbers only"<<endl;
+        return 0;
+    }
+    if(!(HH>=0 && HH<=23)){
+        cout<<"Hour should be greater than or equal to 0 and less than or equal to 23 hours"<<endl;
+        return 0;}
+    if(!(MM>=0 && MM<=59)){
+        cout<<"Minute should be greater than or equal to 0 and less than or equal to 59 minutes"<<endl;
+        return 0;}
+    if(!(h_add>=0 && h_add<=1000)){
+        cout<<"h_add should be greater than or equal to 0 and less than or equal to 1000 hours"<<endl;
+        return 0;}
+    if(!(m_add>=0 && m_add<=10000)){
+        cout<<"m_add should be greater than or equal to 0 and less than or equal to 10000 minutes"<<endl;
+        return 0;}
     
-    hourincrement+=(MM/60);
+    MM = MM + m_add;
+    h_add=h_add+(MM/60);
     MM=MM%60;
-    HH=HH+hourincrement;
+    HH=HH+h_add;
     HH=HH%24;
-    cout<<HH<<":"<<MM;
- 
     
+    cout<<setfill('0')<<setw(2)<<HH<<":"<<setw(2)<<MM;
+
     return 0;
 }
