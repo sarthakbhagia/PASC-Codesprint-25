8f0a51762af05e05da632d69946e8bb2784079c9
sarthakbhagia <sarthakbhagia1@gmail.com>
2025-11-23 21:22:11 +0530

    Update AddHoursAndMinutesToTime.cpp

--- AddHoursAndMinutesToTime.cpp
+++ AddHoursAndMinutesToTime.cpp
@@ -31,13 +31,18 @@ int main() {
     if(!(m_add>=0 && m_add<=10000)){
         cout<<"m_add should be greater than or equal to 0 and less than or equal to 10000 minutes"<<endl;
         return 0;}
-    
     MM = MM + m_add;
+    //cout<<"minute added is="<<MM<<endl;
+    //cout<<"(MM/60)="<<(MM/60)<<endl;
+    //cout<<"h_add before="<<h_add<<endl;
     h_add=h_add+(MM/60);
+    //cout<<"h_add after="<<h_add<<endl;
     MM=MM%60;
+    //cout<<"MM%60="<<MM%60<<endl;
     HH=HH+h_add;
+    //cout<<"HH afer="<<HH<<endl;
     HH=HH%24;
-    
+    //cout<<"HH%24="<<HH%24<<endl;
     cout<<setfill('0')<<setw(2)<<HH<<":"<<setw(2)<<MM;
 
     return 0;
